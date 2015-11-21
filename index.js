da862fb6f717ce1be4e791a3f7b9322a4c392e19
Steve McCarthy <steve@redlua.com>
2015-11-21 18:27:41 -0500

    Add testing output file

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var stringify = require('csv-stringify');
+var fs = require('fs');
+var output = fs.createWriteStream('testing.csv');
 
-stringifier = stringify()
+stringifier = stringify();
 stringifier.on('readable', function(){
-  while(data = stringifier.read()){
-    // append to file
-    process.stdout.write(data);
+  while(data = stringifier.read()) {
+    output.write(data);
   }
 });
 stringifier.on('error', function(err){
@@ -12,14 +13,12 @@ stringifier.on('error', function(err){
 });
 stringifier.on('finish', function(){
   console.log("Complete!");
-  // list all images that failed to parse properly?
 });
 
 // write headers first
 stringifier.write(['order', 'file_path', 'thumbnail', 'capture_uuid', 'page_uri', 'book_uri', 'source_rotated', 'width', 'height', 'source_x', 'source_y', 'source_w' ,'source_h']);
 
 // load all images, parse, and write info
-
 // TODO implement this part
 
 
